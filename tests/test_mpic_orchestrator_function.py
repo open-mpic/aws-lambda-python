a08127e86ca891ffe5821de4bc1af58b1da4f6fc
Dmitry Sharkov <dsharkov@gmail.com>
2024-07-18 20:51:23 -0400

    added project.toml and hatch. may still remove

--- tests/test_mpic_orchestrator_function.py
+++ tests/test_mpic_orchestrator_function.py
@@ -1,8 +1,12 @@
-import unittest
+import pytest
 
-class MyTestCase(unittest.TestCase):
+from aws_lambda_python.lambda_controller import lambda_function
+
+
+class TestMpicOrchestrator:
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
