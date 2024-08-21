7f6da1c2b959c00f7fc191b93f68dedf0b5923b4
Dmitry Sharkov <dsharkov@gmail.com>
2024-08-21 01:02:17 -0400

    writing new classes to hold parameters for the checking lambdas

--- src/aws_lambda_python/common_domain/caa_check_request.py
+++ src/aws_lambda_python/common_domain/caa_check_request.py
@@ -1,11 +1,12 @@
-from dataclasses import dataclass
-from typing import Optional
-
 from aws_lambda_python.common_domain.certificate_type import CertificateType
+from pydantic import BaseModel
 
 
-@dataclass
-class CaaCheckRequest:
+class CaaCheckRequestCaaDetails(BaseModel):
+    certificate_type: CertificateType | None = None
+    caa_domains: list[str] | None = None
+
+
+class CaaCheckRequest(BaseModel):
     domain_or_ip_target: str
-    certificate_type: CertificateType
-    caa_domains: Optional[list[str]]
+    caa_details: CaaCheckRequestCaaDetails | None = None
--- src/aws_lambda_python/common_domain/dcv_check_request.py
+++ src/aws_lambda_python/common_domain/dcv_check_request.py
@@ -1,19 +1,20 @@
-from dataclasses import dataclass
-from typing import Optional
-
 from aws_lambda_python.common_domain.dcv_validation_method import DcvValidationMethod
 from aws_lambda_python.common_domain.dns_record_type import DnsRecordType
+from pydantic import BaseModel
 
 
-@dataclass
-class DcvCheckRequest:
-    @dataclass
-    class DcvCheckRequestValidationDetails:
-        prefix: Optional[str]
-        record_type: Optional[DnsRecordType]
-        path: Optional[str]
-        expected_challenge: str
+class DcvCheckRequestValidationDetails:
+    prefix: str | None = None
+    record_type: DnsRecordType | None = None
+    path: str | None = None
+    expected_challenge: str
 
-    domain_or_ip_target: str
+
+class DcvCheckRequestDcvDetails(BaseModel):
     validation_method: DcvValidationMethod
-    validation_details: Optional[DcvCheckRequestValidationDetails]
+    validation_details: DcvCheckRequestValidationDetails
+
+
+class DcvCheckRequest(BaseModel):
+    domain_or_ip_target: str
+    dcv_details: DcvCheckRequestDcvDetails
