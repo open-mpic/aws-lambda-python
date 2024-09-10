866486f8a8142a049e5dd448cf462e32b503cfa5
Henry Birge-Lee <henrybirgelee@yahoo.com>
2024-09-10 14:51:39 -0400

    better

--- src/aws_lambda_python/mpic_dcv_checker/mpic_dcv_checker.py
+++ src/aws_lambda_python/mpic_dcv_checker/mpic_dcv_checker.py
@@ -14,7 +14,9 @@ from aws_lambda_python.common_domain.validation_error import ValidationError
 # noinspection PyUnusedLocal
 class MpicDcvChecker:
     def __init__(self):
+        self.rir_region: Final[str] = os.environ['rir_region']
         self.AWS_REGION: Final[str] = os.environ['AWS_REGION']
+
 
     def check_dcv(self, event):
         dcv_request = DcvCheckRequest.model_validate(event)
@@ -39,17 +41,19 @@ class MpicDcvChecker:
 
         response = requests.get(token_url)
 
+        perspective_name = self.rir_region + "." + self.AWS_REGION
+
         if response.status_code == requests.codes.OK:
             result = response.text.strip()
             dcv_check_response = DcvCheckResponse(
-                perspective=self.AWS_REGION,
+                perspective=perspective_name,
                 check_passed=(result == expected_response_content),
                 timestamp_ns=time.time_ns(),
                 details=DcvCheckResponseDetails()  # FIXME get details
             )
         else:
             dcv_check_response = DcvCheckResponse(
-                perspective=self.AWS_REGION,
+                perspective=perspective_name,
                 check_passed=False,
                 timestamp_ns=time.time_ns(),
                 errors=[ValidationError(error_type=str(response.status_code), error_message=response.reason)],
@@ -63,6 +67,9 @@ class MpicDcvChecker:
         }
 
     def perform_dns_validation(self, request):
+        perspective_name = self.rir_region + "." + self.AWS_REGION
+
+        
         domain_or_ip_target = request.domain_or_ip_target  # TODO iterate up through parent domains to base domain?
         dns_name_prefix = request.dcv_check_parameters.validation_details.dns_name_prefix
         dns_record_type = dns.rdatatype.from_text(request.dcv_check_parameters.validation_details.dns_record_type)
@@ -89,7 +96,7 @@ class MpicDcvChecker:
                             records_as_strings.append(record_data_as_string)
 
             dcv_check_response = DcvCheckResponse(
-                perspective=self.AWS_REGION,
+                perspective=perspective_name,
                 check_passed=expected_dns_record_content in records_as_strings,
                 timestamp_ns=time.time_ns(),
                 details=DcvCheckResponseDetails()  # FIXME get details (or don't bother with this)
@@ -101,7 +108,7 @@ class MpicDcvChecker:
             }
         except dns.exception.DNSException as e:
             dcv_check_response = DcvCheckResponse(
-                perspective=self.AWS_REGION,
+                perspective=perspective_name,
                 check_passed=False,
                 timestamp_ns=time.time_ns(),
                 errors=[ValidationError(error_type=e.__class__.__name__, error_message=e.msg)],
