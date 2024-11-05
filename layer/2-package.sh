b74d6410ca5bfd1ef41820d42007f8161e44da29
Dmitry Sharkov <dsharkov@gmail.com>
2024-11-05 15:58:00 -0500

    updated paths for open_mpic_core layer components for packaging the layer

--- layer/2-package.sh
+++ layer/2-package.sh
@@ -10,7 +10,7 @@ py_exclude=('*.pyc' '*__pycache__*')
 
 # make mpic_common lambda layer for all lambda functions
 mkdir -p mpic_common_layer_content/python/open_mpic_core
-cp -r ../src/open_mpic_core/common_domain mpic_common_layer_content/python/open_mpic_core/common_domain
+cp -r ../open_mpic_core_python/src/open_mpic_core/common_domain mpic_common_layer_content/python/open_mpic_core/common_domain
 cd mpic_common_layer_content
 zip -r ../mpic_common_layer_content.zip python -x "${py_exclude[@]}" # Zip the mpic_common lambda layer
 rm -r python # clean up, mostly not to bother the IDE which will find this duplicate code!
@@ -18,7 +18,7 @@ cd .. # should be at layer directory
 
 # make mpic_coordinator lambda layer for mpic coordinator lambda function
 mkdir -p mpic_coordinator_layer_content/python/open_mpic_core
-cp -r ../src/open_mpic_core/mpic_coordinator mpic_coordinator_layer_content/python/open_mpic_core/mpic_coordinator
+cp -r ../open_mpic_core_python/src/open_mpic_core/mpic_coordinator mpic_coordinator_layer_content/python/open_mpic_core/mpic_coordinator
 cp -r ../resources mpic_coordinator_layer_content/python/resources  # TODO consider a more elegant approach
 cd mpic_coordinator_layer_content
 zip -r ../mpic_coordinator_layer_content.zip python -x "${py_exclude[@]}" # Zip the mpic_coordinator lambda layer
@@ -27,7 +27,7 @@ cd .. # should be at layer directory
 
 # make mpic_caa_checker lambda layer for mpic caa checker lambda function
 mkdir -p mpic_caa_checker_layer_content/python/open_mpic_core
-cp -r ../src/open_mpic_core/mpic_caa_checker mpic_caa_checker_layer_content/python/open_mpic_core/mpic_caa_checker
+cp -r ../open_mpic_core_python/src/open_mpic_core/mpic_caa_checker mpic_caa_checker_layer_content/python/open_mpic_core/mpic_caa_checker
 cd mpic_caa_checker_layer_content
 zip -r ../mpic_caa_checker_layer_content.zip python -x "${py_exclude[@]}" # Zip the mpic_caa_checker lambda layer
 rm -r python # clean up, mostly not to bother the IDE which will find this duplicate code!
@@ -35,7 +35,7 @@ cd .. # should be at layer directory
 
 # make mpic_dcv_checker lambda layer for mpic dcv checker lambda function
 mkdir -p mpic_dcv_checker_layer_content/python/open_mpic_core
-cp -r ../src/open_mpic_core/mpic_dcv_checker mpic_dcv_checker_layer_content/python/open_mpic_core/mpic_dcv_checker
+cp -r ../open_mpic_core_python/src/open_mpic_core/mpic_dcv_checker mpic_dcv_checker_layer_content/python/open_mpic_core/mpic_dcv_checker
 cd mpic_dcv_checker_layer_content
 zip -r ../mpic_dcv_checker_layer_content.zip python -x "${py_exclude[@]}" # Zip the mpic_dcv_checker lambda layer
 rm -r python # clean up, mostly not to bother the IDE which will find this duplicate code!
