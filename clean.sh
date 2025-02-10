1606b53f81c627b384c1abeddd2042e34ef164ee
Henry Birge-Lee <birgelee@princeton.edu>
2025-02-09 19:18:07 -0500

    bump core and cleanup deploy files. fix test

--- clean.sh
+++ clean.sh
@@ -5,11 +5,11 @@ cd $SCRIPT_DIR
 
 FUNCTIONS_DIR="src/aws_lambda_mpic"
 
-rm open-tofu/*.generated.tf
+$(rm open-tofu/*.generated.tf 2> /dev/null) || true
 
-rm layer/*.zip
-rm -r layer/create_layer_virtualenv
+$(rm layer/*.zip 2> /dev/null) || true
+#$(rm -r layer/create_layer_virtualenv 2> /dev/null) || true
 
-rm "${FUNCTIONS_DIR}"/mpic_coordinator_lambda/mpic_coordinator_lambda.zip
-rm "${FUNCTIONS_DIR}"/mpic_caa_checker_lambda/mpic_caa_checker_lambda.zip
-rm "${FUNCTIONS_DIR}"/mpic_dcv_checker_lambda/mpic_dcv_checker_lambda.zip
+$(rm "${FUNCTIONS_DIR}"/mpic_coordinator_lambda/mpic_coordinator_lambda.zip 2> /dev/null) || true
+$(rm "${FUNCTIONS_DIR}"/mpic_caa_checker_lambda/mpic_caa_checker_lambda.zip 2> /dev/null) || true
+$(rm "${FUNCTIONS_DIR}"/mpic_dcv_checker_lambda/mpic_dcv_checker_lambda.zip 2> /dev/null) || true
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./clean.sh; hatch env prune; hatch run lambda-layer:install; ./package-layer.sh; hatch run ./configure.py; ./zip-all.sh; cd open-tofu; tofu apply -var="dnssec_enabled=false" -auto-approve; cd ..
