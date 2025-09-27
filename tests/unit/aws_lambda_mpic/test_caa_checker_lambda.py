e5ea121ead6486b6a25038d0326baa9bb9dea66c
Dmitry Sharkov <dsharkov@gmail.com>
2025-09-27 02:29:32 -0400

    added tests for setting timeout config for CAA from enviroment

--- tests/unit/aws_lambda_mpic/test_caa_checker_lambda.py
+++ tests/unit/aws_lambda_mpic/test_caa_checker_lambda.py
@@ -7,6 +7,7 @@ from open_mpic_core import CaaCheckResponse, CaaCheckResponseDetails
 from open_mpic_core_test.test_util.mock_dns_object_creator import MockDnsObjectCreator
 from open_mpic_core_test.test_util.valid_check_creator import ValidCheckCreator
 import aws_lambda_mpic.mpic_caa_checker_lambda.mpic_caa_checker_lambda_function as mpic_caa_checker_lambda_function
+from aws_lambda_mpic.mpic_caa_checker_lambda.mpic_caa_checker_lambda_function import MpicCaaCheckerLambdaHandler
 
 
 # noinspection PyMethodMayBeStatic
@@ -14,7 +15,13 @@ class TestCaaCheckerLambda:
     @staticmethod
     @pytest.fixture(scope="class")
     def set_env_variables():
-        envvars = {"AWS_REGION": "us-east-1", "default_caa_domains": "ca1.com|ca2.org|ca3.net", "log_level": "TRACE"}
+        envvars = {
+            "AWS_REGION": "us-east-1",
+            "default_caa_domains": "ca1.com|ca2.org|ca3.net",
+            "log_level": "TRACE",
+            "dns_timeout_seconds": "12",
+            "dns_resolution_lifetime_seconds": "13",
+        }
         with pytest.MonkeyPatch.context() as class_scoped_monkeypatch:
             for k, v in envvars.items():
                 class_scoped_monkeypatch.setenv(k, v)
@@ -47,10 +54,15 @@ class TestCaaCheckerLambda:
         log_contents = setup_logging.getvalue()
         assert all(text in log_contents for text in ["MpicCaaChecker", "TRACE"])  # Verify the log level was set
 
+    async def lambda_handler__should_set_dns_timeout_configuration_of_dcv_checker(self, set_env_variables):
+        # a bit leaky in terms of implementation but there's not an easy way to enforce this otherwise
+        configured_caa_checker = MpicCaaCheckerLambdaHandler().caa_checker
+        assert configured_caa_checker.resolver.timeout == 12.0
+        assert configured_caa_checker.resolver.lifetime == 13.0
+
     @staticmethod
     def create_caa_check_response():
         return CaaCheckResponse(
-            perspective_code="us-east-1",
             check_passed=True,
             details=CaaCheckResponseDetails(caa_record_present=True, found_at="example.com"),
             timestamp_ns=time.time_ns(),
